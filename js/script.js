b1b7cf38183c57f0cc8f6589fbd5f40d10273045
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-03-22 21:41:49 +0300

    Сетка шапки, стили для текста слайдера. Добавлен файл js для реаризации логики переключения темы

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.page');
+document.querySelector('.slider-controll__btn');
+console.log(document.querySelector('.page'));
+console.log(document.querySelector('.slider-controll__btn'));
