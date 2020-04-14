65479f59746961aa1c9c89432e9d085985202a4b
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-04-15 01:12:45 +0300

    Фокус в поле ввода имени при открытии модальной формы

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@ let feedbackFormLink = document.querySelector(".contacts__button");
 let popup = document.querySelector(".modal-feedback");
 let overlay = document.querySelector(".modal-overlay");
 let modalClose = document.querySelector(".modal__close-btn");
+let feedbackName = popup.querySelector("[name=name]");
 
 feedbackFormLink.addEventListener("click", function (event) {
   event.preventDefault();
   popup.classList.add("modal-show");
   overlay.classList.add("modal-show");
+
+  feedbackName.focus();
 });
 
 modalClose.addEventListener("click", function(event) {
