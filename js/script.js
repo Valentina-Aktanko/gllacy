ed9ce5ca2bde164f4b5d741bb32470a605314985
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-04-15 00:19:44 +0300

    Закрытие popup при нажатии на крестик в форме

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 let feedbackFormLink = document.querySelector(".contacts__button");
+let popup = document.querySelector(".modal-feedback");
+let overlay = document.querySelector(".modal-overlay");
+let modalClose = document.querySelector(".modal__close-btn");
 
 feedbackFormLink.addEventListener("click", function (event) {
   event.preventDefault();
-  
-  let popup = document.querySelector(".modal-feedback");
-  let overlay = document.querySelector(".modal-overlay");
-  
   popup.classList.add("modal-show");
   overlay.classList.add("modal-show");
+});
+
+modalClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-show");
+  overlay.classList.remove("modal-show");
 });
