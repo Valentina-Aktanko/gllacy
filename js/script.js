bf596ef37e332205c245d0ea21830fb8b62536b2
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-04-15 00:08:47 +0300

    Появление popup и overlay при нажатии на кнопку Форма обратной связи

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ let feedbackFormLink = document.querySelector(".contacts__button");
 feedbackFormLink.addEventListener("click", function (event) {
   event.preventDefault();
   
-  let popap = document.querySelector(".modal-feedback");
-  console.log(popap);
+  let popup = document.querySelector(".modal-feedback");
+  let overlay = document.querySelector(".modal-overlay");
   
+  popup.classList.add("modal-show");
+  overlay.classList.add("modal-show");
 });
