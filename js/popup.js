4a4571c8dc4b1fa067c40ba94657e9ac84c5df2c
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-04-17 20:22:07 +0300

    Переименован файл с кодом для popup и подключен на главной странице

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,14 @@
 let feedbackFormButton = document.querySelector(".contacts__button");
+
 let popupFeedback = document.querySelector(".modal-feedback");
 let overlay = document.querySelector(".modal-overlay");
 let modalClose = document.querySelector(".modal__close-btn");
+
 let feedbackForm = popupFeedback.querySelector(".feedback-form");
 let feedbackName = popupFeedback.querySelector("[name=name]");
 let feedbackEmail = popupFeedback.querySelector("[name=email]");
 let feedbackText = popupFeedback.querySelector("[name=text]");
-// let storageName = localStorage.getItem("name");
-// let storageEmail = localStorage.getItem("email");
+
 let isStorageSupport = true;
 let storageName = "";
 let storageEmail = "";
@@ -19,6 +20,7 @@ try {
   isStorageSupport = false;
 }
 
+// Открывает popup с формой и overlay
 feedbackFormButton.addEventListener("click", function (event) {
   event.preventDefault();
   popupFeedback.classList.add("modal-show");
@@ -35,9 +37,9 @@ feedbackFormButton.addEventListener("click", function (event) {
     feedbackEmail.value = storageEmail;
     feedbackText.focus();
   }
-  
 });
 
+// Закрывает popup с формой и overlay
 modalClose.addEventListener("click", function(event) {
   event.preventDefault();
   popupFeedback.classList.remove("modal-show");
@@ -45,6 +47,7 @@ modalClose.addEventListener("click", function(event) {
   overlay.classList.remove("overlay-show");
 });
 
+// Поведение при отправке формы при незаполненном поле
 feedbackForm.addEventListener("submit", function(event) {
   if (!feedbackName.value || !feedbackEmail.value) {
     event.preventDefault();
@@ -57,6 +60,7 @@ feedbackForm.addEventListener("submit", function(event) {
   }
 });
 
+// Закрытие popup и overlay при нажатии Esc
 window.addEventListener("keydown", function (event) {
   if (event.keyCode === 27) {
     if (popupFeedback.classList.contains("modal-show") && overlay.classList.contains("overlay-show")) {
