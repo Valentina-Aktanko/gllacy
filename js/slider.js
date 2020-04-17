46c64b7e2b3eef8516ed2777b21ef6d38b19e234
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-04-17 21:30:44 +0300

    js с кодом для слайдера разбит на функции

--- js/slider.js
+++ js/slider.js
@@ -1,74 +1,101 @@
-let page = document.querySelector(".page");
 let slider = document.querySelector(".special-offers");
 
-let slide1 = slider.querySelector(".special-offers__item:first-child");
-let slide2 = slider.querySelector(".special-offers__item:nth-child(2)");
-let slide3 = slider.querySelector(".special-offers__item:nth-child(3)");
+let toggleSlide1 = slider.querySelector("#toggle-theme-green"); 
+let toggleSlide2 = slider.querySelector("#toggle-theme-blue"); 
+let toggleSlide3 = slider.querySelector("#toggle-theme-pink"); 
 
-let sliderControll1 = slider.querySelector(".slider-controll__item:first-child");
-let sliderControll2 = slider.querySelector(".slider-controll__item:nth-child(2)");
-let sliderControll3 = slider.querySelector(".slider-controll__item:nth-child(3)");
+function changeTheme(theme) {
+  let page = document.querySelector(".page");
+  let slider = document.querySelector(".special-offers");
 
-let toggleThemeGreen = slider.querySelector("#toggle-theme-green"); 
-let toggleThemeBlue = slider.querySelector("#toggle-theme-blue"); 
-let toggleThemePink = slider.querySelector("#toggle-theme-pink"); 
+  if (theme === "theme-green") {
+    page.classList.remove("theme-blue");
+    page.classList.remove("theme-pink");
+    page.classList.add("theme-green");
+    slider.classList.remove("theme-blue");
+    slider.classList.remove("theme-pink");
+    slider.classList.add("theme-green");
+  } else if (theme === "theme-blue") {
+    page.classList.remove("theme-green");
+    page.classList.remove("theme-pink");
+    page.classList.add("theme-blue");
+    slider.classList.remove("theme-green");
+    slider.classList.remove("theme-pink");
+    slider.classList.add("theme-blue");
+  } else if (theme === "theme-pink") {
+    page.classList.remove("theme-green");
+    page.classList.remove("theme-blue");
+    page.classList.add("theme-pink");
+    slider.classList.remove("theme-green");
+    slider.classList.remove("theme-blue");
+    slider.classList.add("theme-pink");
+  }
+}
 
-toggleThemeGreen.addEventListener("click", function (event) {
+function changeSlide(currentSlide) {
+  let slider = document.querySelector(".special-offers");
+  
+  let slide1 = slider.querySelector(".special-offers__item:first-child");
+  let slide2 = slider.querySelector(".special-offers__item:nth-child(2)");
+  let slide3 = slider.querySelector(".special-offers__item:nth-child(3)");
+
+  if (currentSlide === "slide1") {
+    slide2.classList.remove("special-offers__item_current");
+    slide3.classList.remove("special-offers__item_current");
+    slide1.classList.add("special-offers__item_current");
+  } else if (currentSlide === "slide2") {
+    slide1.classList.remove("special-offers__item_current");
+    slide3.classList.remove("special-offers__item_current");
+    slide2.classList.add("special-offers__item_current");
+  } else if (currentSlide === "slide3") {
+    slide1.classList.remove("special-offers__item_current");
+    slide2.classList.remove("special-offers__item_current");
+    slide3.classList.add("special-offers__item_current");
+  }
+}
+
+function changeCurrentControll(currentSlide) {
+  let slider = document.querySelector(".special-offers");
+
+  let controll1 = slider.querySelector(".slider-controll__item:first-child");
+  let controll2 = slider.querySelector(".slider-controll__item:nth-child(2)");
+  let controll3 = slider.querySelector(".slider-controll__item:nth-child(3)");
+
+  if (currentSlide === "slide1") {
+    controll2.classList.remove("slider-controll__item_current");
+    controll3.classList.remove("slider-controll__item_current");
+    controll1.classList.add("slider-controll__item_current");
+  } else if (currentSlide === "slide2") {
+    controll1.classList.remove("slider-controll__item_current");
+    controll3.classList.remove("slider-controll__item_current");
+    controll2.classList.add("slider-controll__item_current");
+  } else if (currentSlide === "slide3") {
+    controll1.classList.remove("slider-controll__item_current");
+    controll2.classList.remove("slider-controll__item_current");
+    controll3.classList.add("slider-controll__item_current");
+  }
+}
+
+toggleSlide1.addEventListener("click", function (event) {
   event.preventDefault();
 
-  page.classList.remove("theme-blue");
-  page.classList.remove("theme-pink");
-  page.classList.add("theme-green");
-
-  slider.classList.remove("theme-blue");
-  slider.classList.remove("theme-pink");
-  slider.classList.add("theme-green");
-
-  slide2.classList.remove("special-offers__item_current");
-  slide3.classList.remove("special-offers__item_current");
-  slide1.classList.add("special-offers__item_current");
-  
-  sliderControll2.classList.remove("slider-controll__item_current");
-  sliderControll3.classList.remove("slider-controll__item_current");
-  sliderControll1.classList.add("slider-controll__item_current");
+  changeTheme("theme-green");
+  changeSlide("slide1");
+  changeCurrentControll("slide1");
 });
 
-toggleThemeBlue.addEventListener("click", function (event) {
+toggleSlide2.addEventListener("click", function (event) {
   event.preventDefault();
 
-  page.classList.remove("theme-green");
-  page.classList.remove("theme-pink");
-  page.classList.add("theme-blue");
-
-  slider.classList.remove("theme-green");
-  slider.classList.remove("theme-pink");
-  slider.classList.add("theme-blue");
-
-  slide1.classList.remove("special-offers__item_current");
-  slide3.classList.remove("special-offers__item_current");
-  slide2.classList.add("special-offers__item_current");
-  
-  sliderControll1.classList.remove("slider-controll__item_current");
-  sliderControll3.classList.remove("slider-controll__item_current");
-  sliderControll2.classList.add("slider-controll__item_current");
+  changeTheme("theme-blue");
+  changeSlide("slide2");
+  changeCurrentControll("slide2");
 });
 
-toggleThemePink.addEventListener("click", function (event) {
+toggleSlide3.addEventListener("click", function (event) {
   event.preventDefault();
 
-  page.classList.remove("theme-greem");
-  page.classList.remove("theme-blue");
-  page.classList.add("theme-pink");
-
-  slider.classList.remove("theme-greem");
-  slider.classList.remove("theme-blue");
-  slider.classList.add("theme-pink")
-
-  slide1.classList.remove("special-offers__item_current");
-  slide2.classList.remove("special-offers__item_current");
-  slide3.classList.add("special-offers__item_current");
-  
-  sliderControll1.classList.remove("slider-controll__item_current");
-  sliderControll2.classList.remove("slider-controll__item_current");
-  sliderControll3.classList.add("slider-controll__item_current");
+  changeTheme("theme-pink");
+  changeSlide("slide3");
+  changeCurrentControll("slide3");
 });
