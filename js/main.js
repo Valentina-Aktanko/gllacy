65c9b5427fbb7234725644ad70c48e1f4e86dfc1
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-08-11 12:53:59 +0300

    Остлеживание клика по заголовку Малинка даром в Яндекс метрике

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let targetHeading = document.getElementById("raspberries-for-free-heading");
+
+function handlerHeadingClick() {
+  ym(66146167,'reachGoal',' raspberries-for-free-heading');
+}
+
+targetHeading.addEventListener('click', handlerHeadingClick);
